56fcbe7ebfff79cb2a6a990132dc7429cc043365
XFelipeX <felipediasamorimpessoa@gmail.com>
2023-04-12 13:02:19 -0300

    update sw

---
+++ sw.js
@@ -0,0 +1,71 @@
+const CACHE = 'v1';
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(CACHE);
+  await cache.addAll(resources);
+};
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      '/',
+      '/index.html',
+      '/js/file.js',
+      '/js/main.js',
+      '/js/storage.js',
+      '/js/task.js',
+      '/js/topic.js',
+      '/js/util.js',
+      '/css/style.css',
+      '/css/modal.css',
+    ]),
+  );
+});
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  try {
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: '/offline.html',
+    }),
+  );
+});
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
